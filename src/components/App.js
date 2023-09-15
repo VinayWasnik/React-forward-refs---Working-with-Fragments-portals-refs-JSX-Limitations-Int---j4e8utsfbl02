192b56e5138adb44851f0739a71c74318d882c57
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-09-15 12:19:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React ,{useRef,useState,useEffect} from 'react'
+import React ,{ useRef,useState } from 'react'
 import '../styles/App.css';
 import InputField from './InputField.js';
 const App = () => {
@@ -20,7 +20,7 @@ const App = () => {
  
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/><br/>
+    <InputField ref={inputRef}  type="text"/><br/>
     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
     <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
      <textarea id="textarea" value={values}></textarea>
